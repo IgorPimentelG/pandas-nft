0cdef354e2a149552c9b66f0fde3b528b1db127f
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-07-24 22:22:33 -0300

    🎉 initial project structure

---
+++ frontend/pandas-nft-web/src/App.tsx
@@ -0,0 +1,15 @@
+import Routes from '@routes/routes';
+import Theme from '@theme';
+import { Fragment } from 'react';
+
+function App() {
+	return (
+		<Fragment>
+			<Theme>
+				<Routes/>
+			</Theme>
+		</Fragment>
+	);
+}
+
+export default App;

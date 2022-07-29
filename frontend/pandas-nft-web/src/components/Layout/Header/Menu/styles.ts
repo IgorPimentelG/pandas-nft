f7b12db19f9ae9b4c379249cd7466561a319d604
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-07-28 21:39:09 -0300

    💄 add animation to menu item line

--- frontend/pandas-nft-web/src/components/Layout/Header/Menu/styles.ts
+++ frontend/pandas-nft-web/src/components/Layout/Header/Menu/styles.ts
@@ -1,8 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { LabelProps } from './props';
 
-interface LabelProps {
-	isActive: boolean;
-}
+const animationLine = keyframes`
+	from {
+		width: 0%;
+	}
+
+	to {
+		width: 50%;
+	}
+`;
 
 export const Container = styled.nav`
 	display: flex;
@@ -17,9 +24,9 @@ export const Label = styled.span<LabelProps>`
 `;
 
 export const Line = styled.div`
-	width: 50%;
 	margin-top: 8px;
 	margin-left: 10px;
 	border-radius: 50px;
 	border: 1px solid ${({theme}) => theme.colors.main.primary};
+	animation: ${animationLine} 0.5s both;
 `;
